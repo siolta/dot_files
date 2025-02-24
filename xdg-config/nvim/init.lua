e0a630983763acf6c99f2ec20efdf39d3fea4b12
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-02-24 01:53:42 -0800

    organize nvim mini plugin

--- xdg-config/nvim/init.lua
+++ xdg-config/nvim/init.lua
@@ -3,8 +3,18 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
--- add modular configurations:
-require 'keymaps'
-require 'plugins.lazy'
-require 'autocmds'
+
+-- Set to true if you have a Nerd Font installed and selected on the terminal
+vim.g.have_nerd_font = true
+
+-- [[ Setting options ]]
 require 'options'
+
+-- [[ Setting Keymaps ]]
+require 'keymaps'
+
+-- [[ Install `lazy.vim` plugin manager ]]
+require 'plugins.lazy-bootstrap'
+
+-- [[ Configure and install plugins ]]
+require 'plugins.lazy-plugins'
