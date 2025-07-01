819032d55bd53caa3eba478e93bf0755f9c3fd8a
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-07-01 15:41:55 -0700

    nvim transparent

--- xdg-config/nvim/lua/plugins/theme.lua
+++ xdg-config/nvim/lua/plugins/theme.lua
@@ -8,6 +8,19 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
+      require('tokyonight').setup {
+        transparent = true, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          -- comments = { italic = true },
+          style = 'night',
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = 'transparent', -- style for sidebars, see below
+          floats = 'transparent', -- style for floating windows
+        },
+      }
       -- Load the colorscheme here
       vim.cmd.colorscheme 'tokyonight-night'
 
