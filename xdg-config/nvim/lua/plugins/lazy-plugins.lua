505b180565ed5177dd90a326619caa7ac4a5f6da
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-09-04 16:50:10 -0700

    add obsidian nvim plugin

--- xdg-config/nvim/lua/plugins/lazy-plugins.lua
+++ xdg-config/nvim/lua/plugins/lazy-plugins.lua
@@ -76,6 +76,9 @@ require('lazy').setup({
   -- Theme
   require 'plugins.theme',
 
+  -- Obsidian tools
+  require 'plugins.obsidian',
+
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
